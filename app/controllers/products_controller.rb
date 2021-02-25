7536295cb9480bfc20db6ca5ba725293d2cd167f
Jorge Soto <djsmoothzouk@gmail.com>
2021-02-24 21:20:31 -0600

    Reviews index method fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
-
+  
   # GET /products
   def index
     @products = Product.all
@@ -10,33 +9,11 @@ class ProductsController < ApplicationController
 
   # GET /products/1
   def show
-    render json: @product
+    @product = Product.find(params[:id])
+
+    render json: @product, status: 200
   end
 
-  # POST /products
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /products/1
-  def destroy
-    @product.destroy
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -44,8 +21,4 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :price, :img)
-    end
 end
